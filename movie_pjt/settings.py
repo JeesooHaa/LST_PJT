6af36a8fb1fd8b937ba9bd4962a5cbeba723781c
JeesooHaa <haajeesoo@gmail.com>
2019-11-28 11:16:13 +0900

    staticfiles

--- movie_pjt/settings.py
+++ movie_pjt/settings.py
@@ -130,6 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'accounts.User'
